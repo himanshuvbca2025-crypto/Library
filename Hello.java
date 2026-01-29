56e6d8f51beb152d72864eb80723315d262711de
Himanshu Verma <himanshuv.bca2025@ssism.org>
2026-01-29 21:16:52 +0530

    Himanshu Ke Changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
+        Scanner   sc = new Scanner(System.in);
+        System.out.println("Enter the Number ");
+        int num = sc.nextInt();
+
+
         System.out.println("Hello, World!");
     }
 }
